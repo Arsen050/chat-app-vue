580f90d8496cd56809b20742626c22e2d9dbf2a9
unknown <eskenderovar@yandex.ru>
2022-06-11 14:46:47 +0300

    configured .env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,16 +2,19 @@ import firebase from "firebase";
 import "firebase/firestore";
 import "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDJ36yBfd5cPC8Bjq3_itd2mpkdwYe-vnU",
-  authDomain: "chat-vue-d206f.firebaseapp.com",
-  projectId: "chat-vue-d206f",
-  storageBucket: "chat-vue-d206f.appspot.com",
-  messagingSenderId: "1096873940861",
-  appId: "1:1096873940861:web:2f1847223378b9067c5e14",
-};
+const firebaseConfig = firebase.initializeApp({
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+});
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 const firestore = firebase.firestore();
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 const auth = firebase.auth();
